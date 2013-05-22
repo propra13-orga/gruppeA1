754e194a9e6120fba9983823333826ead8ad1218
Dorine <Dorine.Simiyu@uni-duesseldorf.de>
2013-05-22 16:26:12 -0700

    aktuel Frame

--- src/Frame.java
+++ src/Frame.java
@@ -1,86 +1,83 @@
+package testing;
 
-
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
-public class Frame extends JFrame implements ActionListener{
+public class Frame extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JButton schliessen;
-	private JButton Options;
-	private JButton Info;
+	private static JButton einstellungen;
+	private JButton info;
 	private JButton exit;
 	
+	
 	public static void main(String[] args) {
-		Frame frame = new Frame("Menu");
+		
+		Frame frame = new Frame("Menü");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400,400);
 		frame.setLayout(null);
 		frame.setVisible(true);
-	}
 
-	public Frame (String title){
-		super (title);
-		
-		schliessen = new JButton("Spiel Starten");
-		schliessen.setBounds(120,40,160,40);
-		schliessen.addActionListener(this);
-		add(schliessen);
-		
-		Options = new JButton("einstellungen");
-		Options.setBounds(120,120,160,40);
-		Options.addActionListener(this);
-		add(Options);
-		
-		Info = new JButton("Credits");
-		Info.setBounds(120,200,160,40);
-		Info.addActionListener(this);
-		add(Info);
-		
-		exit = new JButton("Spiel beenden");
-		exit.setBounds(120,280,160,40);
-		exit.addActionListener(this);
-		add(exit);
-			
 	}
-
-	private void auswahl() {
-		// TODO Auto-generated method stub
+			public  Frame(String title){ 
+					super(title);
 		
-	}
-
-	private void fenster() {
-		JFrame fenster = new JFrame();
-		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fenster.setSize(800, 800);
-		fenster.setVisible(true);
-		fenster.setLayout(null);
+						schliessen = new JButton("Spiel Starten");
+						schliessen.setBounds(120,40,160,40);
+						schliessen.addActionListener(this);
+						add(schliessen);
 		
-	}
-	@Override
-	public void actionPerformed(ActionEvent action) {
+						einstellungen = new JButton("Optionen");
+						einstellungen.setBounds(120,90,160,40);
+						einstellungen.addActionListener(this);
+						add(einstellungen);
 		
-		if (action.getSource()== schliessen){
-			fenster();
-		}
+						info = new JButton("Credits");
+						info.setBounds(120,140,160,40);
+						info.addActionListener(this);
+						add(info);
 		
-		if (action.getSource()== Options){
-			auswahl();
-		}
+						exit = new JButton("Spiel verlassen");
+						exit.addActionListener(this);
+						exit.setBounds(120,190,160,40);
+						add(exit);
 		
-		if (action.getSource()== Info){
-			Object[] options = {"ok"};
-			
-			JOptionPane.showOptionDialog(null, "programmiert von Dorine Nanjala, Tim Wachsener, Alexander Schmelter, Alida Wolner und Alexandra Karina!","Information",JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,null,options,options[0]);
-		}
 		
-		if (action.getSource()== exit){
-			System.exit(0);
-		}
-		
-	}	
-	}
+			}
+			public void actionPerformed(ActionEvent e){
+							
+						if(e.getSource() == schliessen){
+								fenster();
+						}
+						if(e.getSource() == info){
+							Object[] options = {"OK"};
+							
+							JOptionPane.showOptionDialog(null,"Das Spiel wurde von Dorine Simiyu, Tim Wachsener, Alexander Schmelter, Alida Wolner und Alexandra Karina programmiert!!","Credits",JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE,null,options,options[0]);
+						}
+						if(e.getSource() == einstellungen){
+							auswahl();
+							
+						}
+						if(e.getSource() == exit){
+							System.exit(0);
+						}
+				}
+				public static void fenster(){
+								JFrame fenster = new JFrame("Dungeon Crawler Deluxe");
+								fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+								fenster.setSize(1000,675);
+								fenster.setVisible(true);
+								fenster.add(new Gui());
+				}
+				public static void auswahl(){
+								
+								JFrame auswahl = new JFrame("Einstellungen");
+								auswahl.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+								auswahl.setSize(400,400);
+								auswahl.setVisible(true);
+				}
+}
